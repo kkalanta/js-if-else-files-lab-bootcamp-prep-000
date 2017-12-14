f859d14913112dad2b799955ee564ccb6a100556
Kimia Kalantari <kimiakalantari1356@gmail.com>
2017-12-14 15:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = "Joe"
-var height = 70
+var name = "Susan"
+var height = 74
 
 if (name === "Susan"){
   console.log("The name is correct")
@@ -7,7 +7,7 @@ if (name === "Susan"){
   console.log("Expected the name to be Susan")
 }
 
-if (height === 70){
+if (height === 74){
   console.log("The height is correct")
 } else {
   console.log("Expected height to be 74")
